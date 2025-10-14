29f8c5639668e6348236d9a712093163f51cc3a2
FrontEndExplorer-90 <janina_mey@hotmail.com>
2025-10-14 20:27:21 +0200

    changes to repo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { createMenu } from "../js/ui/common/createMenu.js";
-import { registerFormListener } from "../js/listeners/auth/registerFormListener.js";
-import { loginFormListener } from "../js/listeners/auth/loginFormListener.js";
+import { registerFormListener } from "./listeners/auth/registerFormListener.js";
+import { loginFormListener } from "./listeners/auth/loginFormListener.js";
 import { logoutButtonListener } from "./listeners/auth/logoutButtonListener.js";
 import { displayVenueList } from "./listeners/venues/displayVenueList.js";
 import { displayVenue } from "./listeners/venues/displayVenue.js";
@@ -10,17 +10,20 @@ function initializeApp() {
   logoutButtonListener();
 
   const path = window.location.pathname;
-  console.log(path);
+  console.log("Current path:", path);
+
 
   if (path === "/" || path === "/index.html") {
     displayVenueList();
-  } else if (path.startsWith("/login")) {
+  } else if (path === "/login/" || path === "/login/index.html") {
     loginFormListener();
-  } else if (path.startsWith("/register")) {
+  } else if (path === "/register/" || path === "/register/index.html") {
     registerFormListener();
-  } else if (path.startsWith("/venue/")) {
+  } else if (path.startsWith("/venue")) {
     displayVenue();
   }
+
 }
 
 initializeApp();
+loginFormListener();
